18e24bb2c40bae1306faf3ab2036a0a57559a9bc
J <744693884@qq.com>
2021-04-08 03:43:57 +0000

    initialize the project

---
+++ libfuzzer/Logger.cpp
@@ -0,0 +1,41 @@
+#include "Logger.h"
+
+using namespace std;
+
+namespace fuzzer {
+  ofstream Logger::debugFile = ofstream("debug.txt", ios_base::app);
+  ofstream Logger::infoFile = ofstream("info.txt", ios_base::app);
+  ofstream Logger::resFile = ofstream("outcome.txt", ios_base::app);
+
+  
+
+  bool Logger::enabled = false;
+
+  void Logger::debug(string str) {
+    if (enabled) {
+      debugFile << str << endl;
+    }
+  }
+
+  void Logger::info(string str) {
+    if (enabled) {
+      infoFile << str << endl;
+    }
+  }
+
+ void Logger::res(string str) {
+    if (enabled) {
+      resFile << str << " ";
+    }
+  }
+  string Logger::testFormat(bytes data) {
+    auto idx = 0;
+    stringstream ss;
+    while (idx < data.size()) {
+      bytes d(data.begin() + idx, data.begin() + idx + 32);
+      idx += 32;
+      ss << toHex(d) << endl;
+    }
+    return ss.str();
+  }
+}
